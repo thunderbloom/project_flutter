88a847aecdfc86af41084121eb7f60902f2701d5
jdk <jeongdukki@gmail.com>
2022-12-16 12:40:46 +0900

    lll

---
+++ lib/models/history_item.dart
@@ -0,0 +1,16 @@
+class Historyitem {
+  dynamic id;
+  String serial_number;
+  String topic;
+  String status;
+  String Datetime;
+
+  Historyitem({
+    this.id,
+    required this.serial_number,
+    required this.topic,
+    required this.status,
+    required this.Datetime
+  });
+
+}
---
+++ lib/providers/history_page.dart
@@ -0,0 +1,20 @@
+import 'dart:convert'; // json 변환
+
+import 'package:flutter/material.dart';
+import 'package:project_flutter/models/history_item.dart';
+import 'package:http/http.dart';
+
+class HistoryNotifier with ChangeNotifier { // 변화(CRUD)가 있을 때 통지
+
+}
+
+  
+  
+
+
+
+
+
+
+  
+}
